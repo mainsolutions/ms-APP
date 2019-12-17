9b7d0442f379c36e49cc608b1d5fa21faebe178e
josejesus <joseaugustolimajesus@gmail.com>
2019-12-17 10:24:04 -0300

    add routes and created navbar component

--- ms-app/src/app/app.module.ts
+++ ms-app/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PaginaInicialComponent
   ],
   imports: [
     BrowserModule,
